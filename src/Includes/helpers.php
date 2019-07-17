84b32f09b7167fe576ddf10b833a64965101c5cc
Chris Mellor <cmellor@gmail.com>
2019-07-17 23:15:01 +0100

    Completely refactored the code
    Added dependency injection
    
    New conditionals to take into account different installation methods.
    
    Completely refactored the code
    
    Added new conditionals
    
    Added a remove command
    
    Added some missing code
    
    Moved code positioning
    
    Renamed PHP `configs` folder

--- src/Includes/helpers.php
+++ src/Includes/helpers.php
@@ -2,116 +2,67 @@
 
 namespace Saber;
 
-use Symfony\Component\Process\Process;
-use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\ConsoleOutput;
-use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Filesystem\Filesystem;
+
+define('SABER_HOME_CONFIG_PATH', $_SERVER['HOME'] . '/.config/saber');
+define('DEFAULT_PHP_VERSION', 7.2);
+define('DEFAULT_DATABASE_IMAGE', 'mariadb:latest');
+
 
 /**
- * Builds the container.
- *
- * @param InputInterface $input
- */
-function buildContainer(InputInterface $input)
-{
-    $output = new ConsoleOutput();
-
-    $progress = new ProgressBar($output, 100);
-    $progress->setBarCharacter('<comment>.</comment>');
-    $progress->setBarWidth(50);
-    $progress->setEmptyBarCharacter('');
-    $progress->setFormat('%message% %bar%');
-    $progress->setProgressCharacter('.');
-
-    $dockerCompose = new Process(['docker-compose', 'up', '-d', '--build']);
-
-    output('<comment>Building your application environment...</comment>');
-
-    try {
-        $progress->setMessage('<comment>Building containers</comment>');
-
-        if ($input->getOption('verbose')) {
-            $dockerCompose->setTimeout(180)->mustRun(function ($type, $line) {
-                output($line);
-            });
-        } else {
-            $progress->start();
-
-            $dockerCompose->setTimeout(180)->mustRun(function () use ($progress) {
-                $progress->advance();
-            });
-        }
-
-        if ($dockerCompose->isSuccessful()) {
-            if (! $input->getOption('verbose')) {
-                $progress->finish();
-                echo PHP_EOL;
-            }
-
-            output('<info>Containers successfully built!</info>');
-        }
-    } catch (ProcessFailedException $error) {
-        echo $error->getMessage();
-    }
-}
-
-/**
- * Restart PHP and NGINX containers
- *
- * @return void
- */
-function restartContainers()
-{
-    output('<comment>Restarting containers...</comment>');
-
-    $dockerRestartContainer = new Process(['docker-compose', 'restart', 'php', 'nginx']);
-    $dockerRestartContainer->run();
-
-    if (! $dockerRestartContainer->isSuccessful()) {
-        throw new ProcessFailedException($dockerRestartContainer);
-    }
-    output('<comment>Containers restarted!</comment>');
-}
-
-/**
- * Shut down and destroy the Docker container and remove the Network.
- */
-function destroyContainer()
-{
-    $dockerComposerDown = new Process(['docker-compose', 'down', '-v']);
-    $dockerComposerDown->run();
-
-    if (! $dockerComposerDown->isSuccessful()) {
-        throw new ProcessFailedException($dockerComposerDown);
-    }
-}
-
-/**
- * Replace text within a file
+ * Replace text within a file.
  *
  * @param string $original
  * @param string $new
  * @param array|string $filename
- *
- * @return void
  */
 function replace($original, $new, $filename)
 {
-    $file = preg_replace('/'.$original.'/', $new, file_get_contents($filename));
+    $file = preg_replace('/' . $original . '/', $new, file_get_contents($filename));
 
     (new Filesystem)->dumpFile($filename, $file);
-
-    return;
 }
 
 /**
- * Output text to the console
+ * Output text related to a successful action
  *
  * @param string $output
- *
  * @return void
+ */
+function success($output)
+{
+    output("<fg=green;options=bold>$output</>");
+}
+
+/**
+ * Output text related to giving valuble information
+ *
+ * @param string $output
+ * @return void
+ */
+function info($output)
+{
+    output("<comment>$output</comment>");
+}
+
+/**
+ * Output text related to a failed action
+ *
+ * @param string $output
+ * @return void
+ */
+function error($output)
+{
+    output("<error>$output</error>");
+
+    exit(1);
+}
+
+/**
+ * Output text to the console.
+ *
+ * @param string $output
  */
 function output($output)
 {
